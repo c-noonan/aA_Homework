ece87d352f018b9aa710e16346bb79216056842b
Katie Noonan <catherine.c.noonan@gmail.com>
2018-01-23 23:51:40 -0800

    Practice with React + Redux components

---
+++ W7D3/Giphy/giphy_search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import { fetchSearchGiphys } from './util/api_util.js';
+import { receiveSearchGiphys } from './reducers/giphys_reducer';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const store = configureStore();
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+});
